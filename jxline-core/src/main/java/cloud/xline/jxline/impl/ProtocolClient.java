0555ee7f26ed7c2e0747e1a7724279d92c968690
iGxnon <igxnon@gmail.com>
2024-02-08 23:12:32 +0800

    feat: implement async curp client
    Signed-off-by: iGxnon <igxnon@gmail.com>

--- jxline-core/src/main/java/cloud/xline/jxline/impl/ProtocolClient.java
+++ jxline-core/src/main/java/cloud/xline/jxline/impl/ProtocolClient.java
@@ -1,19 +1,248 @@
 package cloud.xline.jxline.impl;
 
+import cloud.xline.jxline.exceptions.CurpException;
+import cloud.xline.jxline.exceptions.XlineException;
+import cloud.xline.jxline.utils.Invoke;
+import cloud.xline.jxline.utils.Pair;
 import com.curp.protobuf.*;
+import com.google.protobuf.Empty;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.xline.protobuf.Command;
+import com.xline.protobuf.CommandResponse;
+import com.xline.protobuf.ExecuteError;
+import com.xline.protobuf.SyncResponse;
 import io.grpc.ManagedChannel;
+import io.vertx.core.Future;
 
+import javax.annotation.Nullable;
 import java.net.URI;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.concurrent.CompletableFuture;
+import java.util.UUID;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 class ProtocolClient extends Impl {
 
     private final State state;
+
+    private ProposeId getProposeId() {
+        UUID uuid = UUID.randomUUID();
+        // TODO: obtain from server
+        long clientId = uuid.getMostSignificantBits();
+        // TODO: implement tracker
+        long seqNum = uuid.getMostSignificantBits();
+        return ProposeId.newBuilder().setClientId(clientId).setSeqNum(seqNum).build();
+    }
+
+    <T> CompletableFuture<T> propose(
+            Command cmd,
+            boolean useFastPath,
+            Function<Pair<CommandResponse, SyncResponse>, T> convert) {
+        ProposeId id = this.getProposeId();
+        Executor executor = connectionManager().getExecutorService();
+        if (!useFastPath) {
+            return CompletableFuture.supplyAsync(() -> this.fastRound(id, cmd), executor)
+                    .handleAsync((r, ex) -> this.slowRound(id), executor)
+                    .thenApply(convert);
+        }
+        CompletionService<T> service =
+                new ExecutorCompletionService<>(connectionManager().getExecutorService());
+        service.submit(() -> convert.apply(new Pair<>(this.fastRound(id, cmd), null)));
+        service.submit(() -> convert.apply(this.slowRound(id)));
+
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    CurpException exception = null;
+                    for (int i = 0; true; i++) {
+                        try {
+                            return service.take().get();
+                        } catch (InterruptedException e) {
+                            throw XlineException.toXlineException(e);
+                        } catch (ExecutionException e) {
+                            Throwable cause = e.getCause();
+                            if (cause instanceof XlineException) {
+                                throw (XlineException) cause;
+                            }
+                            if (cause instanceof CurpException) {
+                                CurpException ex = ((CurpException) cause);
+                                if (ex.shouldAbortSlowRound()) {
+                                    throw (CurpException) cause;
+                                }
+                                if (exception == null
+                                        || exception.priority().value() <= ex.priority().value()) {
+                                    exception = ex;
+                                }
+                                if (i == 1) {
+                                    throw exception;
+                                }
+                                continue;
+                            }
+                            throw XlineException.toXlineException(e);
+                        }
+                    }
+                },
+                executor);
+    }
+
+    Pair<CommandResponse, SyncResponse> slowRound(ProposeId id) {
+        logger().info(String.format("Slow round start. Propose ID %s.", id));
+        WaitSyncedRequest waitSyncReq =
+                WaitSyncedRequest.newBuilder()
+                        .setProposeId(id)
+                        .setClusterVersion(this.state.getClusterVersion())
+                        .build();
+        try {
+            WaitSyncedResponse resp =
+                    completable(
+                                    mapLeader(stub -> stub.waitSynced(waitSyncReq)),
+                                    res -> res,
+                                    CurpException::toCurpException)
+                            .get();
+            if (resp.getExeResult().hasError()) {
+                throw new XlineException(ExecuteError.parseFrom(resp.getExeResult().getError()));
+            }
+            if (resp.getAfterSyncResult().hasError()) {
+                throw new XlineException(
+                        ExecuteError.parseFrom(resp.getAfterSyncResult().getError()));
+            }
+            CommandResponse er = CommandResponse.parseFrom(resp.getExeResult().getOk());
+            SyncResponse asr = SyncResponse.parseFrom(resp.getAfterSyncResult().getOk());
+            return new Pair<>(er, asr);
+        } catch (InterruptedException | InvalidProtocolBufferException e) {
+            throw XlineException.toXlineException(e);
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            if (!(cause instanceof CurpException)) {
+                throw XlineException.toXlineException(cause);
+            }
+            throw (CurpException) cause;
+        }
+    }
+
+    /**
+     * Run fastRound
+     *
+     * @param id The Proposal id
+     * @param cmd The command
+     * @return {@link CommandResponse}
+     * @throws {@link XlineException} when got serializing error, command execution error or
+     *     unexpected behavior, {@link CurpException} when got curp error
+     */
+    CommandResponse fastRound(ProposeId id, Command cmd) {
+        logger().info(String.format("Fast round start. Propose ID %s.", id));
+        ProposeRequest propReq =
+                ProposeRequest.newBuilder()
+                        .setCommand(cmd.toByteString())
+                        .setProposeId(id)
+                        .setClusterVersion(this.state.getClusterVersion())
+                        .build();
+
+        Collection<VertxProtocolGrpc.ProtocolVertxStub> stubs = this.state.getStubs().values();
+        int okCnt = 0;
+        int superQuorum = this.superQuorum(stubs.size());
+        CommandResponse exeRes = null;
+        CurpException exception = null;
+
+        CompletionService<CommandResponse> completionService =
+                forEachServer(
+                        stubs,
+                        stub -> {
+                            CmdResult cmdResult =
+                                    completable(
+                                                    stub.propose(propReq),
+                                                    ProposeResponse::getResult,
+                                                    CurpException::toCurpException)
+                                            .get();
+                            if (cmdResult.hasError()) {
+                                ExecuteError error = ExecuteError.parseFrom(cmdResult.getError());
+                                throw new XlineException(error);
+                            }
+                            if (cmdResult.hasOk()) {
+                                return CommandResponse.parseFrom(cmdResult.getOk());
+                            }
+                            return null;
+                        });
+
+        for (int i = 0; i < stubs.size(); i++) {
+            try {
+                CommandResponse resp = completionService.take().get();
+                if (resp != null) {
+                    exeRes = resp;
+                }
+                okCnt++;
+                if (okCnt >= superQuorum && exeRes != null) {
+                    return exeRes;
+                }
+            } catch (InterruptedException e) {
+                // unexpected exception
+                throw XlineException.toXlineException(e);
+            } catch (ExecutionException e) {
+                Throwable cause = e.getCause();
+                if (!(cause instanceof CurpException)) {
+                    throw XlineException.toXlineException(cause);
+                }
+                CurpException ex = (CurpException) cause;
+                if (ex.shouldAbortFastRound()) {
+                    throw ex;
+                }
+                if (exception == null || exception.priority().value() <= ex.priority().value()) {
+                    exception = ex;
+                }
+            }
+        }
+        if (exception != null) {
+            throw exception;
+        }
+        // We will at least send the request to the leader if no `WrongClusterVersion` returned.
+        // If no errors occur, the leader should return the ER
+        // If it is because the super quorum has not been reached, an error will definitely occur.
+        // Otherwise, there is no leader in the cluster state currently, return wrong cluster
+        // version
+        // and attempt to retrieve the cluster state again.
+        throw new CurpException(
+                CurpError.newBuilder().setWrongClusterVersion(Empty.newBuilder().build()).build());
+    }
+
+    <T> CompletionService<T> forEachServer(
+            Collection<VertxProtocolGrpc.ProtocolVertxStub> stubs,
+            Invoke<VertxProtocolGrpc.ProtocolVertxStub, T> task) {
+        CompletionService<T> completionService =
+                new ExecutorCompletionService<>(this.connectionManager().getExecutorService());
+        for (VertxProtocolGrpc.ProtocolVertxStub stub : stubs) {
+            completionService.submit(() -> task.call(stub));
+        }
+        return completionService;
+    }
+
+    <T> Future<T> mapLeader(Function<VertxProtocolGrpc.ProtocolVertxStub, Future<T>> task) {
+        VertxProtocolGrpc.ProtocolVertxStub leaderStub = this.state.getLeaderStub();
+        if (leaderStub == null) {
+            // choose a random leader, it will return redirect error if leader is wrong
+            // TODO: fetch cluster here.
+            leaderStub =
+                    this.state.stubs.values()
+                            .toArray(VertxProtocolGrpc.ProtocolVertxStub[]::new)[0];
+        }
+        return task.apply(leaderStub);
+    }
+
+    int superQuorum(int size) {
+        int faultTolerance = size - quorum(size);
+        return faultTolerance + recoverQuorum(size);
+    }
+
+    int quorum(int size) {
+        return size / 2 + 1;
+    }
+
+    int recoverQuorum(int size) {
+        return quorum(size) / 2 + 1;
+    }
 
     ProtocolClient(ClientConnectionManager connectionManager) {
         super(connectionManager);
@@ -95,6 +324,19 @@ class ProtocolClient extends Impl {
             long res = this.leaderId;
             this.lock.readLock().unlock();
             return res;
+        }
+
+        /**
+         * Get leader stub
+         *
+         * @return Leader stub or null if there are some disjoints in local stubs.
+         */
+        @Nullable
+        VertxProtocolGrpc.ProtocolVertxStub getLeaderStub() {
+            this.lock.readLock().lock();
+            VertxProtocolGrpc.ProtocolVertxStub stub = this.stubs.get(this.leaderId);
+            this.lock.readLock().unlock();
+            return stub;
         }
 
         void checkUpdate(FetchClusterResponse res) {
